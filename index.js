432d8d1241e7b0ac4bc43ee58ea9462b1e55c58e
Shardul Chatufale <shardulschatufale@gmail.com>
2023-09-28 21:28:58 +0530

    committed 6 - module export

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const route = require("./route/route");
+const route = require("./src/route/route");
 const mongoose = require("mongoose");
 const app = express();
 
