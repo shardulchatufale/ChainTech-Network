292f66a38cfc7e99a9d323724d1ae28d75fa0b2a
Shardul Chatufale <shardulschatufale@gmail.com>
2023-09-28 22:08:25 +0530

    committed 7 - mongo url

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // ==+==+==+==+==+==+==+==+==+==[Connect DataBase]==+==+==+==+==+==+==+==+==+==
+let MongoURL = process.env.MongoURL;
 mongoose
-  .connect(
-    "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.1",
-    {
-      useNewUrlParser: true,
-    }
-  )
+  .connect(String(MongoURL), { useNewUrlParser: true, })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err.message));
 
 
 app.use("/", route);
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Port running on " + (process.env.PORT || 3000));
--- src/route/route.js
+++ src/route/route.js
@@ -2,21 +2,19 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controller/taskControllers')
 const userController = require('../controller/userController')
-const middleware=require("../middleware/auth");
+const middleware = require("../middleware/auth");
 
 router.post('/register', userController.UserRegister);
-router.post('/login',userController.UserLogin);
+router.post('/login', userController.UserLogin);
 
-router.post("/createtask",middleware.authenticate, taskController.CreateTask);
-router.get("/getAllTask",middleware.authenticate,taskController.GetAlltask)
-router.get("/marktask",middleware.authenticate, taskController.MarkTask)
-router.put('/EditTask',middleware.authenticate,taskController.EditTask)
-router.delete('/DeleteTask',middleware.authenticate,taskController.DeleteTask)
-
-
-
-
-
-
+router.post("/createtask", middleware.authenticate, taskController.CreateTask);
+router.get("/getAllTask", middleware.authenticate, taskController.GetAlltask)
+router.get("/marktask", middleware.authenticate, taskController.MarkTask)
+router.put('/EditTask', middleware.authenticate, taskController.EditTask)
+router.delete('/DeleteTask', middleware.authenticate, taskController.DeleteTask)
+router.get('/', (req, res) => {
+    console.log("Host Request received @ ", new Date());
+    res.send(JSON.stringify("Server is up !!!"));
+});
 
 module.exports = router
